47695873f373f9be1c01ba8722e35e1e59701002
chandan21choudhary <chandan48choudhary@gmail.com>
2021-07-19 16:52:58 +0530

    first commit

---
+++ src/main/java/com/java/movieinfo/model/Movie.java
@@ -0,0 +1,53 @@
+package com.java.movieinfo.model;
+
+public class Movie {
+	
+	private String movieId;
+
+	private String name;
+	
+
+	public Movie() {
+		super();
+	}
+
+
+	public Movie(String movieId, String name) {
+		super();
+		this.movieId = movieId;
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the movieId
+	 */
+	public String getMovieId() {
+		return movieId;
+	}
+
+
+	/**
+	 * @param movieId the movieId to set
+	 */
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
